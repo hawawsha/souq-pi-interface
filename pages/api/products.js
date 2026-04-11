739d9495bbfa658fa3c5430b6454ed07cca3765d
AHMAD SULEIMAN MUSALLAM AlHawawsheh <38798371+hawawsha@users.noreply.github.com>
2026-04-11 15:03:49 +0300

    Update products.js

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,7 +1,8 @@
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://souqpicebdaa9713.pinet.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
